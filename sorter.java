acef8510e7e18c9ceb22c3d5a172bdf0998ae193
Danah Aljameel <s202153110@kfupm.edu.sa>
2023-09-09 23:19:54 +0300

    insertionSort

--- sorter.java
+++ sorter.java
@@ -1,2 +1,16 @@
-public class sorter {
+public class Sorter {
+    void insertionSort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
 }
