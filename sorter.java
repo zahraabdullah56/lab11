bc4fc7b8e8d9346975ad154b349a15956d5efdb3
Danah Aljameel <s202153110@kfupm.edu.sa>
2023-09-09 23:32:18 +0300

    selection

--- sorter.java
+++ sorter.java
@@ -13,4 +13,20 @@ public class Sorter {
             arr[i] = smallerNumber;
         }
     }
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+
 }
